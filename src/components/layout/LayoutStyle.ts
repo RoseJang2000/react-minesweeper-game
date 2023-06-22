e76b7545d7e8b696fa04fb30a0108c3780c39e95
Rose Jang <jangmi749@gmail.com>
2023-06-22 18:51:27 +0900

    :sparkles: Feat : Layout 컴포넌트 생성, Header 컴포넌트 배치

---
+++ src/components/layout/LayoutStyle.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  padding-top: 7rem;
+  background-color: #fefefe;
+`;
+
+export { Wrapper };
