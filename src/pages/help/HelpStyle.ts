77e4fdf35458b038e5a632b74c0f8527f048f8ff
Rose Jang <jangmi749@gmail.com>
2023-06-23 17:37:08 +0900

    :lipstick: Design : Help 페이지 스타일 설정

---
+++ src/pages/help/HelpStyle.ts
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  width: 70%;
+  margin: auto 0;
+  padding: 3rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #ddd;
+  border-radius: 2rem;
+  gap: 2rem;
+`;
+
+const HelpContent = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+
+  .help-title {
+    font-size: 1.7rem;
+  }
+  .help-desc {
+    width: 100%;
+    text-align: left;
+    margin: 0 1rem;
+    line-height: 1.3rem;
+    list-style: disc;
+  }
+`;
+
+export { Wrapper, HelpContent };
