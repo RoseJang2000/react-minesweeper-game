ed0db5e87fc81072d73a88a81de80ca9ee3ae6e4
Rose Jang <jangmi749@gmail.com>
2023-06-24 06:16:58 +0900

    :sparkles: Feat : 게임 재시작, 타이머 증가 기능 설정

--- src/components/controlBar/ControlBar.tsx
+++ src/components/controlBar/ControlBar.tsx
@@ -1,11 +1,33 @@
+import { useDispatch, useSelector } from 'react-redux';
 import { PlayButton, StyledNumber, Wrapper } from './ControlBarStyle';
+import { RootState } from 'store';
+import { gameSlice } from 'store/modules/game';
+import { useEffect } from 'react';
+import { useInterval } from 'hooks/useInterval';
 
 const ControlBar = () => {
+  const dispatch = useDispatch();
+  const { startGame, increaseTimer } = gameSlice.actions;
+  const { gameStatus, timer, timerStart, mines, flags } = useSelector(
+    (state: RootState) => state.game,
+  );
+  const minesLeft = mines - flags;
+  const delay = timerStart ? 1000 : null;
+
+  const handleGameStart = () => {
+    dispatch(startGame());
+  };
+
+  // 게임이 시작되면 1초마다 타이머 증가시키는 액션 호출
+  useInterval(() => {
+    dispatch(increaseTimer());
+  }, delay);
+
   return (
     <Wrapper>
-      <StyledNumber>💣 0000</StyledNumber>
-      <PlayButton>😀</PlayButton>
-      <StyledNumber>🕒 0000</StyledNumber>
+      <StyledNumber>💣 {String(minesLeft).padStart(4, '0')}</StyledNumber>
+      <PlayButton onClick={handleGameStart}>😀</PlayButton>
+      <StyledNumber>🕒 {String(timer).padStart(4, '0')}</StyledNumber>
     </Wrapper>
   );
 };
