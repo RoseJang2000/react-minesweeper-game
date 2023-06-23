e12dc8206ec8c561ec37a5bfae9f106b4f683f6e
Rose Jang <jangmi749@gmail.com>
2023-06-24 07:19:49 +0900

    :sparkles: Feat : 처음 연 셀이 지뢰가 아니도록 설정

--- src/components/board/Board.tsx
+++ src/components/board/Board.tsx
@@ -2,19 +2,51 @@ import Cell from 'components/cell/Cell';
 import { useDispatch, useSelector } from 'react-redux';
 import { CellsWrapper } from './BoardStyle';
 import { RootState } from 'store';
+import { gameSlice } from 'store/modules/game';
+import { GAME_STATUS } from 'utils/constants';
+
+export type HandleLeftClick = (
+  e: React.MouseEvent<HTMLDivElement>,
+  x: number,
+  y: number,
+) => void;
 
 const Board = () => {
-  const { rowSize, colSize, board } = useSelector(
+  const dispatch = useDispatch();
+  const { gameStatus, rowSize, colSize, board } = useSelector(
     (state: RootState) => state.game,
   );
+  const { startGame, changeCellData, openCell } = gameSlice.actions;
   const sizeArr = Array(rowSize * colSize).fill(null);
+
+  const handleLeftClick = (
+    e: React.MouseEvent<HTMLDivElement>,
+    x: number,
+    y: number,
+  ) => {
+    e.preventDefault();
+    if (board[y][x].mine && gameStatus === GAME_STATUS.READY) {
+      dispatch(startGame());
+      dispatch(openCell({ x, y }));
+      return;
+    }
+    dispatch(openCell({ x, y }));
+  };
 
   return (
     <CellsWrapper rowSize={rowSize} colSize={colSize}>
       {sizeArr.map((_, idx) => {
         const x = idx % rowSize; // 열
         const y = Math.floor(idx / rowSize); // 행
-        return <Cell key={idx} x={x} y={y} cellData={board[y][x]} />;
+        return (
+          <Cell
+            key={idx}
+            x={x}
+            y={y}
+            cellData={board[y][x]}
+            handleLeftClick={handleLeftClick}
+          />
+        );
       })}
     </CellsWrapper>
   );
--- src/components/cell/Cell.tsx
+++ src/components/cell/Cell.tsx
@@ -1,3 +1,4 @@
+import { HandleLeftClick } from 'components/board/Board';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from 'store';
 import { CellState, gameSlice } from 'store/modules/game';
@@ -8,11 +9,12 @@ interface CellProps {
   x: number;
   y: number;
   cellData: CellState;
+  handleLeftClick: HandleLeftClick;
 }
 
-const Cell = ({ x, y, cellData }: CellProps) => {
+const Cell = ({ x, y, cellData, handleLeftClick }: CellProps) => {
   const dispatch = useDispatch();
-  const { changeCellData, openCell } = gameSlice.actions;
+  const { changeCellData } = gameSlice.actions;
   const { gameStatus } = useSelector((state: RootState) => state.game);
 
   const handleRightClick = (e: React.MouseEvent<HTMLDivElement>) => {
@@ -20,16 +22,12 @@ const Cell = ({ x, y, cellData }: CellProps) => {
     dispatch(changeCellData({ x, y }));
   };
 
-  const handleLeftClick = (e: React.MouseEvent<HTMLDivElement>) => {
-    e.preventDefault();
-    dispatch(openCell({ x, y }));
-  };
-
   const handleCellText = () => {
     if (cellData.isOpen)
       return cellData.aroundMines === 0 ? '' : cellData.aroundMines;
     if (gameStatus === GAME_STATUS.WIN && cellData.mine) return '💣';
     if (gameStatus === GAME_STATUS.LOSE && cellData.mine) return '💥';
+    if (cellData.mine) return 'X';
     if (cellData.question) return '?';
     if (cellData.flag) return '🚩';
     return;
@@ -47,7 +45,7 @@ const Cell = ({ x, y, cellData }: CellProps) => {
     <StyledCell
       className={cellData.isOpen ? 'cell-opened' : ''}
       onClick={(e) => {
-        handleLeftClick(e);
+        handleLeftClick(e, x, y);
       }}
       onContextMenu={(e) => {
         handleRightClick(e);
