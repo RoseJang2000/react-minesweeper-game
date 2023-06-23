1a0c21986b5c2e57f7a6767cc2f1d96789e280e8
Rose Jang <jangmi749@gmail.com>
2023-06-24 01:13:33 +0900

    :sparkles: Feat : 생성한 보드 Board 컴포넌트에 적용

--- src/components/board/Board.tsx
+++ src/components/board/Board.tsx
@@ -1,15 +1,22 @@
 import Cell from 'components/cell/Cell';
+import { useDispatch, useSelector } from 'react-redux';
 import { CellsWrapper } from './BoardStyle';
+import { RootState } from 'store';
 
 const Board = () => {
-  const rowSize = 8;
-  const colSize = 8;
-  const testArr = Array(rowSize * colSize).fill(null);
+  const dispatch = useDispatch();
+  const { rowSize, colSize, board } = useSelector(
+    (state: RootState) => state.game,
+  );
+  const sizeArr = Array(rowSize * colSize).fill(null);
+
   return (
     <CellsWrapper rowSize={rowSize} colSize={colSize}>
-      {testArr.map((_, i) => (
-        <Cell key={i} />
-      ))}
+      {sizeArr.map((_, idx) => {
+        const x = idx % rowSize; // 열
+        const y = Math.floor(idx / colSize); // 행
+        return <Cell key={idx} />;
+      })}
     </CellsWrapper>
   );
 };
