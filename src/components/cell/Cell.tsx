a8b9b1a41abe42f8bc07229e59660fc45259a980
Rose Jang <jangmi749@gmail.com>
2023-06-24 06:48:10 +0900

    :lipstick: Design : 셀 상태, 게임 상태 별 셀 디자인

--- src/components/cell/Cell.tsx
+++ src/components/cell/Cell.tsx
@@ -1,5 +1,7 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from 'store';
 import { CellState, gameSlice } from 'store/modules/game';
+import { GAME_STATUS } from 'utils/constants';
 import { StyledCell } from './CellStyle';
 
 interface CellProps {
@@ -11,6 +13,7 @@ interface CellProps {
 const Cell = ({ x, y, cellData }: CellProps) => {
   const dispatch = useDispatch();
   const { changeCellData, openCell } = gameSlice.actions;
+  const { gameStatus } = useSelector((state: RootState) => state.game);
 
   const handleRightClick = (e: React.MouseEvent<HTMLDivElement>) => {
     e.preventDefault();
@@ -22,6 +25,24 @@ const Cell = ({ x, y, cellData }: CellProps) => {
     dispatch(openCell({ x, y }));
   };
 
+  const handleCellText = () => {
+    if (cellData.isOpen)
+      return cellData.aroundMines === 0 ? '' : cellData.aroundMines;
+    if (gameStatus === GAME_STATUS.WIN && cellData.mine) return '💣';
+    if (gameStatus === GAME_STATUS.LOSE && cellData.mine) return '💥';
+    if (cellData.question) return '?';
+    if (cellData.flag) return '🚩';
+    return;
+  };
+
+  const handleCellColor = () => {
+    if (cellData.aroundMines) {
+      if (cellData.aroundMines >= 3) return 'red';
+      if (cellData.aroundMines === 2) return 'orange';
+    }
+    return 'green';
+  };
+
   return (
     <StyledCell
       className={cellData.isOpen ? 'cell-opened' : ''}
@@ -31,14 +52,10 @@ const Cell = ({ x, y, cellData }: CellProps) => {
       onContextMenu={(e) => {
         handleRightClick(e);
       }}
+      isOpen={cellData.isOpen}
+      textColor={handleCellColor()}
     >
-      {cellData.isOpen
-        ? cellData.aroundMines
-        : cellData.flag
-        ? '🚩'
-        : cellData.question
-        ? '?'
-        : ''}
+      {handleCellText()}
     </StyledCell>
   );
 };
