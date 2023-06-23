85d783395c5836c563c017742d03c4ccd55c9b00
Rose Jang <jangmi749@gmail.com>
2023-06-24 01:38:53 +0900

    :bug: Fix : 보드 세팅 지뢰 개수 오류 수정

--- src/utils/gameControl.ts
+++ src/utils/gameControl.ts
@@ -1,3 +1,4 @@
+import Board from 'components/board/Board';
 import { CellState } from 'store/modules/game';
 
 export const boardSetting = (
@@ -12,7 +13,7 @@ export const boardSetting = (
   const result: CellState[][] = []; // 완성된 보드를 저장할 2차원 배열
 
   // 지뢰 개수만큼 랜덤한 숫자를 추출
-  while (randomNumbers.length === mines) {
+  while (randomNumbers.length !== mines) {
     // idxArr 길이 사이의 랜덤한 인덱스를 추첨
     const randomIdx = Math.floor(idxArr.length * Math.random());
     // 위에서 추출한 랜덤한 인덱스를 이용해 idxArr의 해당 순서의 숫자를 splice로 추출해 중복 제거
@@ -36,7 +37,7 @@ export const boardSetting = (
   for (const mineNumber of randomNumbers) {
     const x = mineNumber % rowSize;
     const y = Math.floor(mineNumber / rowSize);
-    result[y][x].mine = true;
+    result[y][x] = { ...result[y][x], mine: true };
   }
 
   return result;
