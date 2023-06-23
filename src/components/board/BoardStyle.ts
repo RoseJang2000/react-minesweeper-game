bbf35de107fbe6b6a6a6c520560fef73fbcc9663
Rose Jang <jangmi749@gmail.com>
2023-06-23 20:49:12 +0900

    :lipstick: Design : 게임 보드 overflow 설정

--- src/components/board/BoardStyle.ts
+++ src/components/board/BoardStyle.ts
@@ -2,10 +2,14 @@ import styled from 'styled-components';
 import { CELL_SIZE } from 'utils/constants';
 
 const CellsWrapper = styled.div<{ rowSize: number; colSize: number }>`
+  width: fit-content;
+  max-width: 100%;
+  margin: auto;
   display: grid;
   grid-template-columns: repeat(${(props) => props.rowSize}, ${CELL_SIZE}rem);
   grid-template-rows: repeat(${(props) => props.colSize}, ${CELL_SIZE}rem);
   gap: 0.2rem;
+  overflow: auto;
 `;
 
 export { CellsWrapper };
